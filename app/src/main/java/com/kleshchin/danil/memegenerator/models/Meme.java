cfbacf640caf13b9884292ce6ccaeaba6c8772e9
Danil Kleshchin <danil.kleshchin@gmail.com>
2017-09-11 10:41:57 +0300

    Create meme model

---
+++ app/src/main/java/com/kleshchin/danil/memegenerator/models/Meme.java
@@ -0,0 +1,13 @@
+package com.kleshchin.danil.memegenerator.models;
+
+/**
+ * Created by Danil Kleshchin on 11.09.2017.
+ */
+
+public class Meme {
+    public String name;
+    public int id;
+    public String url;
+    public String width;
+    public String height;
+}
